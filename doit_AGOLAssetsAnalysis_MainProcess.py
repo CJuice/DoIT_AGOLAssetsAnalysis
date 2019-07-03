25a220eb50e1b6adc7730172db3f54c07065cebb
CJuice <CJuice@users.noreply.github.com>
2019-07-03 11:08:06 -0400

    process working

--- doit_AGOLAssetsAnalysis_MainProcess.py
+++ doit_AGOLAssetsAnalysis_MainProcess.py
@@ -13,10 +13,10 @@ def main():
     print(f"Start Time: {start_time} seconds since Epoch")
 
     import configparser
-    import datetime
     import pandas as pd
     import requests
 
+    from arcgis.gis import GIS
     from doit_AGOLAssetsAnalysis_Utility import Utility
     from doit_AGOLAssetsAnalysis_DatasetAGOL import DatasetAGOL
 
@@ -27,40 +27,70 @@ def main():
 
     # VARIABLES
     parser = configparser.ConfigParser()
-    today_date = datetime.datetime.now().strftime("%Y%m%d")
-    summary_json_file = r"Docs\ProcessOutputs\{today_date}_AGOL_Assets_Inventory_Summary.csv".format(today_date=today_date)
-    # FUNCTIONS
+    md_doit_agol_credentials_config = r"Docs\md_doit_agol_credentials.cfg"
+    parser.read(filenames=[md_doit_agol_credentials_config])
+    agol_password = parser["AGOL"]["PASSWORD"]
+    agol_root_url = parser["AGOL"]["ROOT_URL"]
+    agol_username = parser["AGOL"]["USER_NAME"]
+    agol_layer_id = parser["AGOL"]["LAYER_ID"]
 
     # FUNCTIONALITY
+    # make a request to the data catalog and get the type value of each asset
     master_list_of_results = DatasetAGOL.request_all_data_catalog_results()
-
-    # Print outs for general understanding of data.json level process
-    print(f"Data Catalog Results Requests Process Completed... {Utility.calculate_time_taken(start_time=start_time)} seconds since start")
-    print(f"RESULTS: {len(master_list_of_results)} ")
     type_list = []
     for item in master_list_of_results:
         type_ = item.get("type", None) if item is not None else None
         type_list.append(type_)
 
-    # Evaluate asset types and summarize
+    # Print outs for general understanding of data.json level process
+    print(f"Data Catalog Results Requests Process Completed... {Utility.calculate_time_taken(start_time=start_time)} seconds since start")
+    print(f"RESULTS: {len(master_list_of_results)} ")
+
+    # Need to get set of unique asset types, and perform summary on values
     counts_ser = pd.Series(data=type_list)
-    value_counts = counts_ser.value_counts()
-    sum = value_counts.sum()
+    value_counts = counts_ser.value_counts()  # Unique values and the number of times they occur
+    total_number_of_assets_encountered = value_counts.sum()
     value_counts_dict = value_counts.to_dict()
-    value_counts_dict["TOTAL"] = [sum]  # Put into a list so that can create a dataframe directly from passing dict
-    summary_df = pd.DataFrame(value_counts_dict)
-    summary_df.to_csv(path_or_buf=summary_json_file, index=False)
 
-    # For print out purposes
-    print(summary_df)
-    exit()
+    # Need the total number of assets encountered in the available values for display so add it to the dict
+    value_counts_dict["TOTAL"] = total_number_of_assets_encountered
+
+    # This portion requires the intepreter to be ESRI ArcGIS Pro python
     # Upserting results to hosted table
-    agol_password = parser["AGOL"]["PASSWORD"]
-    agol_root_url = parser["AGOL"]["ROOT_URL"]
-    agol_username = parser["AGOL"]["USER_NAME"]
-    agol_layer_id = parser["AGOL"]["LAYER_ID"]
-    print(f"AGOL variable parsing complete. Time passed since start = {datetime.now() - start_time}")
 
+    # Need an agol connection session thingy
+    gis = GIS(url=agol_root_url, username=agol_username, password=agol_password)
+    print(f"AGOL connection established... {Utility.calculate_time_taken(start_time=start_time)} seconds since start")
+
+    # Need to get the hosted feature table based on id.
+    asset_inventory_table_agol = gis.content.get(agol_layer_id)
+    asset_inventory_tables_list = asset_inventory_table_agol.tables
+    asset_inventory_table = asset_inventory_tables_list[0]
+
+    # Need to get feature set for layer, isolate record, and change attribute value. Used ESRI dev docs for guidance
+    try:
+        asset_inventory_feature_set = asset_inventory_table.query()
+        asset_inventory_features_list = asset_inventory_feature_set.features
+
+        if len(asset_inventory_features_list) != 1:
+            print(f"WARNING: More than one feature in the truck count feature layer. Expected 1\n{asset_inventory_features_list}")
+            exit()
+
+        first_record = asset_inventory_features_list[0]
+        headers_to_skip = ("ObjectId",)
+        headers_in_table = first_record.attributes.keys() - headers_to_skip
+        for header in headers_in_table:
+            new_header = header.replace("_", " ")
+            first_record.attributes[header] = value_counts_dict.get(new_header, None)  # PRODUCTION
+            # first_record.attributes[header] = -9999  # TESTING
+
+        # Need to change the existing count value to the newest value pulled from the database
+        update_result = asset_inventory_table.edit_features(updates=[asset_inventory_features_list[0]])
+        print(f"Result of update: {update_result}")
+
+    except RuntimeError as rte:
+        print(f"Runtime Error raised: {rte}")
+        exit()
 
     print(f"\nProcess Completed... {Utility.calculate_time_taken(start_time=start_time)} seconds since start")
 
