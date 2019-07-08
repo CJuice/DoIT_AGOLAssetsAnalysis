53a5cad41f9eccb932209cc87ece1cb008cdba77
CJuice <CJuice@users.noreply.github.com>
2019-07-08 14:39:31 -0400

    deleting unnecessary functionality from data freshness process

--- doit_AGOLAssetsAnalysis_Utility.py
+++ doit_AGOLAssetsAnalysis_Utility.py
@@ -1,15 +1,13 @@
 """
-A class of static utility methods independent of the process type (socrata or agol).
+A class of static utility methods
 Author: CJuice
 Date: 20190702
 Modifications:
 
 """
 
-import configparser
 import requests
 import time
-import xml.etree.ElementTree as ET
 
 
 class Utility:
@@ -58,17 +56,6 @@ class Utility:
             return response
 
     @staticmethod
-    def setup_config(cfg_file: str) -> configparser.ConfigParser:
-        """
-        Instantiate the parser for accessing a config file.
-        :param cfg_file: config file to access
-        :return:
-        """
-        cfg_parser = configparser.ConfigParser()
-        cfg_parser.read(filenames=cfg_file)
-        return cfg_parser
-
-    @staticmethod
     def calculate_time_taken(start_time: float) -> float:
         """
         Calculate the time difference between now and the value passed as the start time
@@ -77,47 +64,3 @@ class Utility:
         :return: Difference value between start time and current time
         """
         return time.time() - start_time
-
-    @staticmethod
-    def extract_all_immediate_child_features_from_element(element: ET.Element, tag_name: str):
-        """
-        Extract all immediate children of the element provided to the method.
-
-        :param element: ET.Element of interest to be interrogated
-        :param tag_name: tag of interest on which to search
-        :return: list of all discovered ET.Element items
-        """
-        try:
-            return element.findall(tag_name)
-        except AttributeError as ae:
-            print(f"AttributeError: Unable to extract '{tag_name}' from {element.text}: {ae}")
-            return None
-
-    @staticmethod
-    def extract_first_immediate_child_feature_from_element(element: ET.Element, tag_name: str):
-        """Extract first immediate child feature from provided xml ET.Element based on provided tag name
-
-        :param element: xml ET.Element to interrogate
-        :param tag_name: name of desired tag
-        :return: ET.Element of interest
-        """
-
-        try:
-            return element.find(tag_name)
-        except AttributeError as ae:
-            print(f"AttributeError: Unable to extract '{tag_name}' from {element.text}: {ae}")
-            return None
-
-    @staticmethod
-    def parse_xml_response_to_element(response_xml_str: str) -> ET.Element:
-        """
-        Process xml response content to xml ET.Element
-        :param response_xml_str: string xml from response
-        :return: xml ET.Element
-        """
-        try:
-            return ET.fromstring(response_xml_str)
-        except Exception as e:  # TODO: Improve exception handling
-            print(f"Unable to process xml response to Element using ET.fromstring(): {e}")
-            exit()
-
